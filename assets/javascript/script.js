3b8f36cf324641e123c9ae655ad4200d0d90c9f7
Maria Francis-Moullier <moullier@gmail.com>
2019-12-07 16:44:12 -0700

    sidebar is functional, but static

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,29 @@
+// declare global variables
+let cityArray = [];
+
 // inital pull of Tucson info to populate the page
 // later, try to incorporate the geolocator API to this
 $.ajax({
     url: "https://api.openweathermap.org/data/2.5/weather?q=Tucson&APPID=7c1d14299c12aa7faada3a48e18af17b&units=imperial",
     method: "GET"
-  }).then(function(response) {
+}).then(function(response) {
 
     console.log(response);
     
     displayCurrentWeather(response);
 
-  });
+});
+
+$.ajax({
+    url: "https://api.openweathermap.org/data/2.5/forecast?q=Tucson&APPID=7c1d14299c12aa7faada3a48e18af17b&units=imperial",
+    method: "GET"
+}).then(function(response) {
+    
+    display5DayForecast(response);
+
+});
+
+populateSidebar();
 
 function displayCurrentWeather(response){
     let city = response.name;
@@ -50,22 +64,69 @@ function displayCurrentWeather(response){
     newWindP.text("Wind Speed: " + wind + " MPH");
 
     // append data to HTML page
+    $(".weatherBody").empty();
     $(".weatherBody").append(newTempP);    
     $(".weatherBody").append(newHumidityP);
     $(".weatherBody").append(newWindP);
 
 }
 
+function display5DayForecast(response) {
+
+    console.log(response);
+
+}
+
+function populateSidebar() {
+
+    cityArray = JSON.parse(localStorage.getItem("cityList") || "[]");
+
+    console.log("cityArray.length = " + cityArray.length);
+    console.log("cityArray = " + cityArray);
+
+
+    if(cityArray.length == 0) {
+        // populate sidebar with default cities
+        cityArray = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Shanghai", "Mexico City", "London"];
+    }
+
+    // build the sidebar
+    for(let i = 0; i < cityArray.length; i++) {
+        let newLI = $("<li>");
+        newLI.addClass("list-group-item sidebarBtn");
+        newLI.attr("dataCity", cityArray[i]);
+        newLI.text(cityArray[i]);
+        $("#sidebarList").append(newLI);
+
+    }
+}
+
 // This function handles events where the search button is clicked
 $("#searchButton").on("click", function(event) {
-    event.preventDefault();
-
+    
     // This line grabs the input from the textbox
     let city = $("#cityInput").val().trim();
     console.log(city);
 
-    let weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=7c1d14299c12aa7faada3a48e18af17b";
-    
+    updatePage(city);
+
+});
+
+// Adding a click event listener to all elements with a class of "sidebarBtn"
+$(document).on("click", ".sidebarBtn", updateFromSidebar);
+
+function updateFromSidebar() {
+
+    let city = $(this).attr("dataCity");
+
+    updatePage(city);
+  
+}
+
+function updatePage(city) {
+    event.preventDefault();
+
+    let weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=7c1d14299c12aa7faada3a48e18af17b&units=imperial";
     
     $.ajax({
         url: weatherQueryURL,
@@ -74,26 +135,18 @@ $("#searchButton").on("click", function(event) {
 
         console.log(response);
     
-        let city = response.name;
-
-        // convert dt to date format
-        console.log(response.dt);
-        console.log(typeof(response.dt));
-        let day = moment.unix(response.dt);
-        console.log(day.format("MM/DD/YYYY"));
-
-        let currentWeatherTitle = $("#weatherTitle");
-        let titleString = city + " (" + day.format("MM/DD/YYYY") + ")";
-        $(currentWeatherTitle).text(titleString);
-        
-        let temp = response.main.temp;
-        let humidity = response.main.humidity;
-    
-    
-        console.log(city);
+        displayCurrentWeather(response);
     
       });
 
+    let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=7c1d14299c12aa7faada3a48e18af17b&units=imperial";
 
-
-});
+      $.ajax({
+        url: forecastQueryURL,
+        method: "GET"
+      }).then(function(response) {
+    
+        display5DayForecast(response);
+    
+      });
+}
